d2128607b79b5cdbd31264e60853831d2f70b666
MatinBrd <145439241+MatinBrd@users.noreply.github.com>
2023-10-12 09:55:03 +0300

    matindwt.py

--- timeflux_example/nodes/matindwt.py
+++ timeflux_example/nodes/matindwt.py
@@ -1,18 +1,21 @@
-import numpy as np
+#import numpy as np
 import pywt as pywt
 from timeflux.core.node import Node
 
 
 class Dwt(Node):
-    
- def __init__(self, levels= 1, funct='db4'):
+    def __init__(self, levels=1, funct='db4'):
      
-     self._levels = levels
-     self._funct= funct
-     self._start = None
-def update(self):
-        timestamp = now()
-        float = time_to_float(timestamp)
-        if self._start is None:
-            self._start = float
-        self.o= pywt.dwt(self.i, mode='symmetric', self._funct,self._levels)
+        self._levels = levels
+        self._funct= funct
+        #self._start = None
+     
+    def update(self):
+        #timestamp = now()
+        #float = time_to_float(timestamp)
+        # if self._start is None:
+        #     self._start = float
+        if not self.i.ready():
+            return
+        cA, cD = pywt.dwt(self.i.data, self._funct,self._levels)
+        self.o.data = cA
